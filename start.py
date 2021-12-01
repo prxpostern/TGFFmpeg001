177bbd1004c41b8a970547fae531cf1003aefac1
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-01 14:19:24 +0330

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient, events, Button
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo, DocumentAttributeFilename
 from download_from_url import download_file, get_size
 from file_handler import send_to_transfersh_async, progress, progressb
 from progress_for_telethon import progress2
